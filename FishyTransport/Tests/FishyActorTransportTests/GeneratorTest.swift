a7bdddb4ba34e2d6dc5cc05843d001e93fb654c6
Ricardo Nogueira <ricardonogueira@mac.com>
2021-09-18 21:52:17 +0100

    Adjust SourceGen and expected code in test to have consistent formatting

--- FishyTransport/Tests/FishyActorTransportTests/GeneratorTest.swift
+++ FishyTransport/Tests/FishyActorTransportTests/GeneratorTest.swift
@@ -78,62 +78,62 @@ private struct TestConstants {
       import func Foundation.sleep
       import struct Foundation.Data
       import class Foundation.JSONDecoder
+      
       extension ChatRoom: FishyActorTransport.MessageRecipient {
         enum _Message: Sendable, Codable {
-      case join(chatter: Chatter)
-      case message(String, from: Chatter)
-      case leave(chatter: Chatter)
-      }
-      nonisolated func _receiveAny<Encoder, Decoder>(
-        envelope: Envelope, encoder: Encoder, decoder: Decoder
-      ) async throws -> Encoder.Output
-        where Encoder: TopLevelEncoder, Decoder: TopLevelDecoder {
-        let message = try decoder.decode(_Message.self, from: envelope.message as! Decoder.Input) // TODO: this needs restructuring to avoid the cast, we need to know what types we work with
-        return try await self._receive(message: message, encoder: encoder)
-      }
-
-      nonisolated func _receive<Encoder>(
-        message: _Message, encoder: Encoder
-      ) async throws -> Encoder.Output where Encoder: TopLevelEncoder {
-        do {
-          switch message {
-      case .join(let chatter):
-      let result = try await self.join(chatter: chatter)
-      return try encoder.encode(result)
-
-      case .message(let message, let chatter):
-      try await self.message(message, from: chatter)
-      return try encoder.encode(Optional<String>.none)
-
-      case .leave(let chatter):
-      try await self.leave(chatter: chatter)
-      return try encoder.encode(Optional<String>.none)
-
+          case join(chatter: Chatter)
+          case message(String, from: Chatter)
+          case leave(chatter: Chatter)
+        }
+        
+        nonisolated func _receiveAny<Encoder, Decoder>(
+          envelope: Envelope, encoder: Encoder, decoder: Decoder
+        ) async throws -> Encoder.Output
+          where Encoder: TopLevelEncoder, Decoder: TopLevelDecoder {
+          let message = try decoder.decode(_Message.self, from: envelope.message as! Decoder.Input) // TODO: this needs restructuring to avoid the cast, we need to know what types we work with
+          return try await self._receive(message: message, encoder: encoder)
+        }
+        
+        nonisolated func _receive<Encoder>(
+          message: _Message, encoder: Encoder
+        ) async throws -> Encoder.Output where Encoder: TopLevelEncoder {
+          do {
+            switch message {
+            case .join(let chatter):
+              let result = try await self.join(chatter: chatter)
+              return try encoder.encode(result)
+      
+            case .message(let message, let chatter):
+              try await self.message(message, from: chatter)
+              return try encoder.encode(Optional<String>.none)
+      
+            case .leave(let chatter):
+              try await self.leave(chatter: chatter)
+              return try encoder.encode(Optional<String>.none)
+            }
+          } catch {
+            fatalError("Error handling not implemented; \(error)")
           }
-        } catch {
-          fatalError("Error handling not implemented; \(error)")
+        }
+        
+        @_dynamicReplacement(for: _remote_join(chatter:))
+        nonisolated func _fishy_join(chatter: Chatter) async throws -> String {
+          let message = Self._Message.join(chatter: chatter)
+          return try await requireFishyTransport.send(message, to: self.id, expecting: String.self)
+        }
+        
+        @_dynamicReplacement(for: _remote_message(_:from:))
+        nonisolated func _fishy_message(_ message: String, from chatter: Chatter) async throws  {
+          let message = Self._Message.message(message, from: chatter)
+          return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
+        }
+        
+        @_dynamicReplacement(for: _remote_leave(chatter:))
+        nonisolated func _fishy_leave(chatter: Chatter) async throws  {
+          let message = Self._Message.leave(chatter: chatter)
+          return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
         }
       }
-          @_dynamicReplacement(for: _remote_join(chatter:))
-          nonisolated func _fishy_join(chatter: Chatter) async throws -> String {
-              let message = Self._Message.join(chatter: chatter)
-              return try await requireFishyTransport.send(message, to: self.id, expecting: String.self)
-          }
-
-          @_dynamicReplacement(for: _remote_message(_:from:))
-          nonisolated func _fishy_message(_ message: String, from chatter: Chatter) async throws  {
-              let message = Self._Message.message(message, from: chatter)
-              return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
-          }
-
-          @_dynamicReplacement(for: _remote_leave(chatter:))
-          nonisolated func _fishy_leave(chatter: Chatter) async throws  {
-              let message = Self._Message.leave(chatter: chatter)
-              return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
-          }
-      }
-
-
       """#,
     ],
     [
@@ -141,70 +141,70 @@ private struct TestConstants {
       // DO NOT MODIFY: This file will be re-generated automatically.
       // Source generated by FishyActorsGenerator (version x.y.z)
       import _Distributed
-
+      
       import FishyActorTransport
       import ArgumentParser
       import Logging
-
+      
       import func Foundation.sleep
       import struct Foundation.Data
       import class Foundation.JSONDecoder
+      
       extension Chatter: FishyActorTransport.MessageRecipient {
         enum _Message: Sendable, Codable {
-      case join(room: ChatRoom)
-      case chatterJoined(room: ChatRoom, chatter: Chatter)
-      case chatRoomMessage(String, from: Chatter)
-      }
-      nonisolated func _receiveAny<Encoder, Decoder>(
-        envelope: Envelope, encoder: Encoder, decoder: Decoder
-      ) async throws -> Encoder.Output
-        where Encoder: TopLevelEncoder, Decoder: TopLevelDecoder {
-        let message = try decoder.decode(_Message.self, from: envelope.message as! Decoder.Input) // TODO: this needs restructuring to avoid the cast, we need to know what types we work with
-        return try await self._receive(message: message, encoder: encoder)
-      }
-
-      nonisolated func _receive<Encoder>(
-        message: _Message, encoder: Encoder
-      ) async throws -> Encoder.Output where Encoder: TopLevelEncoder {
-        do {
-          switch message {
-      case .join(let room):
-      try await self.join(room: room)
-      return try encoder.encode(Optional<String>.none)
-
-      case .chatterJoined(let room, let chatter):
-      try await self.chatterJoined(room: room, chatter: chatter)
-      return try encoder.encode(Optional<String>.none)
-
-      case .chatRoomMessage(let message, let chatter):
-      try await self.chatRoomMessage(message, from: chatter)
-      return try encoder.encode(Optional<String>.none)
-
+          case join(room: ChatRoom)
+          case chatterJoined(room: ChatRoom, chatter: Chatter)
+          case chatRoomMessage(String, from: Chatter)
+        }
+        
+        nonisolated func _receiveAny<Encoder, Decoder>(
+          envelope: Envelope, encoder: Encoder, decoder: Decoder
+        ) async throws -> Encoder.Output
+          where Encoder: TopLevelEncoder, Decoder: TopLevelDecoder {
+          let message = try decoder.decode(_Message.self, from: envelope.message as! Decoder.Input) // TODO: this needs restructuring to avoid the cast, we need to know what types we work with
+          return try await self._receive(message: message, encoder: encoder)
+        }
+        
+        nonisolated func _receive<Encoder>(
+          message: _Message, encoder: Encoder
+        ) async throws -> Encoder.Output where Encoder: TopLevelEncoder {
+          do {
+            switch message {
+            case .join(let room):
+              try await self.join(room: room)
+              return try encoder.encode(Optional<String>.none)
+      
+            case .chatterJoined(let room, let chatter):
+              try await self.chatterJoined(room: room, chatter: chatter)
+              return try encoder.encode(Optional<String>.none)
+      
+            case .chatRoomMessage(let message, let chatter):
+              try await self.chatRoomMessage(message, from: chatter)
+              return try encoder.encode(Optional<String>.none)
+            }
+          } catch {
+            fatalError("Error handling not implemented; \(error)")
           }
-        } catch {
-          fatalError("Error handling not implemented; \(error)")
+        }
+        
+        @_dynamicReplacement(for: _remote_join(room:))
+        nonisolated func _fishy_join(room: ChatRoom) async throws  {
+          let message = Self._Message.join(room: room)
+          return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
+        }
+        
+        @_dynamicReplacement(for: _remote_chatterJoined(room:chatter:))
+        nonisolated func _fishy_chatterJoined(room: ChatRoom, chatter: Chatter) async throws  {
+          let message = Self._Message.chatterJoined(room: room, chatter: chatter)
+          return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
+        }
+        
+        @_dynamicReplacement(for: _remote_chatRoomMessage(_:from:))
+        nonisolated func _fishy_chatRoomMessage(_ message: String, from chatter: Chatter) async throws  {
+          let message = Self._Message.chatRoomMessage(message, from: chatter)
+          return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
         }
       }
-          @_dynamicReplacement(for: _remote_join(room:))
-          nonisolated func _fishy_join(room: ChatRoom) async throws  {
-              let message = Self._Message.join(room: room)
-              return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
-          }
-
-          @_dynamicReplacement(for: _remote_chatterJoined(room:chatter:))
-          nonisolated func _fishy_chatterJoined(room: ChatRoom, chatter: Chatter) async throws  {
-              let message = Self._Message.chatterJoined(room: room, chatter: chatter)
-              return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
-          }
-
-          @_dynamicReplacement(for: _remote_chatRoomMessage(_:from:))
-          nonisolated func _fishy_chatRoomMessage(_ message: String, from chatter: Chatter) async throws  {
-              let message = Self._Message.chatRoomMessage(message, from: chatter)
-              return try await requireFishyTransport.send(message, to: self.id, expecting: Void.self)
-          }
-      }
-
-
       """#,
     ],
   ]
